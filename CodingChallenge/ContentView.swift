6daad723e2aa90d42e574d20431db13a66953e1b
edgar <e.kosyan0073@gmail.com>
2022-09-24 22:01:26 +0400

    Initial Commit

---
+++ CodingChallenge/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  CodingChallenge
+//
+//  Created by Edgar on 24.09.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
