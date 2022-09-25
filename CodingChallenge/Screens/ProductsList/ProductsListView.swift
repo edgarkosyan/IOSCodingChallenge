8549f7df069b043881d04cb77f6d86c3b78e437a
edgar <e.kosyan0073@gmail.com>
2022-09-25 19:54:49 +0400

    Feature: Filtering

--- CodingChallenge/Screens/ProductsList/ProductsListView.swift
+++ CodingChallenge/Screens/ProductsList/ProductsListView.swift
@@ -9,43 +9,67 @@ import SwiftUI
 
 struct ProductsListView: View {
     @ObservedObject var viewModel: ProductsListViewModel
-    @State var shouldShowTextField: Bool = false
     var body: some View {
-        VStack {
-            ZStack(alignment: .trailing) {
-                HStack(alignment: .center) {
-                    Spacer()
-                    Text("Products")
-                        .font(.title)
-                        .padding()
-                    Spacer()
-                }
-                Image(systemName: "magnifyingglass")
-                    .font(.title2)
-                    .bold(shouldShowTextField)
-                    .padding()
-                    .onTapGesture {
-                        withAnimation {
-                            shouldShowTextField.toggle()
-                        }
-                    }
+        VStack(spacing: 0) {
+            navigationView()
+                .background(Color.white)
+                .zIndex(1)
+            if viewModel.shouldShowTextField {
+                TextField("Search", text: $viewModel.textFieldText)
+                    .padding(7)
+                    .background(Color.blue.opacity(0.1))
+                    .foregroundColor(.gray.opacity(0.8))
+                    .cornerRadius(7)
+                    .padding(.horizontal)
+                    .transition(.move(edge: .top))
             }
             ScrollView {
                 LazyVStack(spacing: 0) {
-                    ForEach(viewModel.products) { product in
+                    ForEach(viewModel.filteredProducts) { product in
                         ItemView(product: product)
                             .background(Color.white
                                 .cornerRadius(7)
                                 .shadow(color: .gray.opacity(0.3), radius: 3, x: -2, y: -2)
-                                
+                                        
                             )
-                            
                             .padding()
                     }
                 }
             }
+            .scrollIndicators(.hidden)
+            .scrollDismissesKeyboard(.immediately)
+            .refreshable {
+                Task {
+                    await viewModel.loadData()
+                }
+            }
         }
         .background(Color.white)
+    }
+    
+    func navigationView() -> some View {
+        ZStack(alignment: .trailing) {
+            HStack(alignment: .center) {
+                Spacer()
+                Text("Products")
+                    .font(.title)
+                Spacer()
+            }
+            Image(systemName: "magnifyingglass")
+                .font(.title2)
+                .bold(viewModel.shouldShowTextField)
+                .padding(.trailing)
+                .onTapGesture {
+                    withAnimation {
+                        viewModel.shouldShowTextField.toggle()
+                        
+                    }
+                    if !viewModel.shouldShowTextField {
+                        viewModel.refreshProducts()
+                    }
+                }
+        }
+        .padding(7)
     }
 }
 
